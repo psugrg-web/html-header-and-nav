0371b8f1514ebc493267204485778e61e8af33d9
Przemysław Sulecki <psugrg@gmail.com>
2024-07-02 19:33:14 +0200

    light & dark mode

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 const primaryNav = document.querySelector('.primary-nav');
 const navToggle = document.querySelector('.primary-nav-toggle');
-const body = document.getElementsByTagName('body');
+const body = document.body;
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -13,4 +13,14 @@ navToggle.addEventListener('click', () => {
         primaryNav.setAttribute('data-visible', false);
         body[0].classList.remove('primary-nav-scroll-disable');
     }
+});
+
+const lightThemeButton = document.querySelector('.light-theme');
+lightThemeButton.addEventListener('click', () => {
+    body.classList.replace('dark-mode', 'light-mode');
+});
+
+const darkThemeButton = document.querySelector('.dark-theme');
+darkThemeButton.addEventListener('click', () => {
+    body.classList.replace('light-mode', 'dark-mode');
 });
